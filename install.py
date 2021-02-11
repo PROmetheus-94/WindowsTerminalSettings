b458c07f9a37b07137a71d21cabdd8e4c2cbc4d1
Marc Volkert <m_volkert@outlook.com>
2021-02-11 18:02:37 +0100

    reworked project structure, added python installer

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from shutil import copy
+
+source = Path.cwd().joinpath("res")
+destination = Path(f"{os.environ['USERPROFILE']}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState")
+
+for item in os.listdir(source):
+    copy(source.joinpath(item), destination)
